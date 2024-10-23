df0fa61b21b409f02ef9de2312deb5c15c3eef69
Lesly <lesarayamor@est.utn.ac.cr>
2024-10-23 13:50:00 -0600

    Se agregan atributos
    Se agregan los respectivos de la clase Clientes y sus set y get

--- Clientes/Cliente.java
+++ Clientes/Cliente.java
@@ -9,5 +9,45 @@ package Clientes;
  * @author Student
  */
 public class Cliente {
+    private int id;
+    private String nombre;
+    private String numContacto;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNumContacto() {
+        return numContacto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNumContacto(String numContacto) {
+        this.numContacto = numContacto;
+    }
+
+    public Cliente(int id, String nombre, String numContacto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.numContacto = numContacto;
+    }
+
+    public Cliente() {
+        this(0,"","");
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", numContacto=" + numContacto + '}';
+    }
+    
+    
     
 }
