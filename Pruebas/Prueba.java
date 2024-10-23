2734a9fcf690d15ec21f65a61b7db757224d19b5
Fabiana <linunezgo@est.utn.ac.cr>
2024-10-23 15:51:04 -0600

    Se agregan algunos vehiculos

--- Pruebas/Prueba.java
+++ Pruebas/Prueba.java
@@ -4,10 +4,42 @@
  */
 package Pruebas;
 
+import Modelo.Vehiculo;
+import java.util.ArrayList;
+
 /**
  *
  * @author Student
  */
 public class Prueba {
-    
+    public class Main {  
+    public static void main(String[] args) {  
+        ArrayList<Vehiculo> listaVehiculos = new ArrayList<>();  
+  
+        listaVehiculos.add(new Vehiculo("Toyota", "Corolla", "ABC123"));  
+        listaVehiculos.add(new Vehiculo("Honda", "Civic", "XYZ789"));  
+        listaVehiculos.add(new Vehiculo("Suzuki", "Swift Sport", "LMN456"));  
+
+        System.out.println("Lista de Vehículos:");  
+        for (Vehiculo vehiculo : listaVehiculos) {  
+            System.out.println(vehiculo);  
+        }  
+
+        System.out.println("Total de vehículos en la lista: " + listaVehiculos.size());  
+
+        String matriculaBuscar = "XYZ789";  
+        for (Vehiculo vehiculo : listaVehiculos) {  
+            if (vehiculo.getMatricula().equals(matriculaBuscar)) {  
+                System.out.println("Vehículo encontrado: " + vehiculo);  
+                break;  
+            }  
+        }  
+
+        listaVehiculos.removeIf(v -> v.getMatricula().equals("ABC123"));  
+        System.out.println("Lista de Vehículos después de eliminar uno:");  
+        for (Vehiculo vehiculo : listaVehiculos) {  
+            System.out.println(vehiculo);  
+        }  
+    }  
+}
 }
