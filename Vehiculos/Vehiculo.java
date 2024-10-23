9efc159e13a93d5ff1648bc2ee0580ce3a1b7bf3
Thyfanny Maryel Martínez Gómez <thmartinezgo@est.utn.ac.cr>
2024-10-23 13:43:37 -0600

    Se crean los metodos get

--- Vehiculos/Vehiculo.java
+++ Vehiculos/Vehiculo.java
@@ -12,7 +12,29 @@ public class Vehiculo {
     private String marca;
     private String modelo;
     private String matricula;
-    
 
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public Vehiculo(String marca, String modelo, String matricula) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+    }
+
+    public Vehiculo() {
+        this("","","");
+    }
+    
+    
 
 }
