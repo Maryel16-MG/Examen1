dcae4b1118e15f2ba6c3c213cace47fa36c10529
Thyfanny Maryel Martínez Gómez <thmartinezgo@est.utn.ac.cr>
2024-10-23 13:34:04 -0600

    Se crea la clase vehiculos

---
+++ Vehiculos/Vehiculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehiculos;
+
+/**
+ *
+ * @author Student
+ */
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private String matricula;
+    
+
+
+}
